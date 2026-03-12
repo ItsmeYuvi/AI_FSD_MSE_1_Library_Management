d946995e51ba4613c452b697993e970102f8a53f
ItsmeYuvi <yuvrajsngh205001@gmail.com>
2026-03-12 12:02:54 +0530

    Fix controller filename case for Render

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const controller = require("../controllers/tempController");
+const controller = require("../controllers/bookController");
 
 router.post("/books", controller.addBook);
 
